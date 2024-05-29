6d11480c7e0a5579b374f3a5a5bbc825e6c56d6e
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2024-05-29 09:03:16 +0100

    Navbar component

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,7 +1,27 @@
-import { NavigationMenu, NavigationMenuList } from "@radix-ui/react-navigation-menu";
+"use client";
+
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu";
+import { NavigationMenu, NavigationMenuItem, NavigationMenuLink, NavigationMenuList } from "@radix-ui/react-navigation-menu";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+
+export const navigationItems = [
+    {
+        name: 'Home',
+        href: '/'
+    },
+    {
+        name: 'Guestbook',
+        href: '/gurstbook'
+    },
+    {
+        name: 'Projects',
+        href: '/projects'
+    }
+]
 
 export function Navbar() {
+    const pathname = usePathname();
     return (
         <nav className="max-w-7xl mx-auto px-4 md:px-8 py-5 grid grid-cols-12">
             <div className="col-span-6 flex md:col-span-3">
@@ -13,7 +33,17 @@ export function Navbar() {
             </div>
             <div className="hidden sm:flex justify-center items-center col-span-6">     
                 <NavigationMenu>
-                    <NavigationMenuList></NavigationMenuList>
+                    <NavigationMenuList>
+                        {navigationItems.map((item, index) => (
+                            <NavigationMenuItem key={index}>
+                                <Link href={item.href} legacyBehavior passHref>
+                                    <NavigationMenuLink active={pathname === item.href} className={navigationMenuTriggerStyle()}>
+                                        {(item.name)}
+                                    </NavigationMenuLink>
+                                </Link>
+                            </NavigationMenuItem>
+                        ))}
+                    </NavigationMenuList>
                 </NavigationMenu>
             </div>
         </nav>
